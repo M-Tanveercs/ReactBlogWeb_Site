00fd2d80d8f5a0220908ede7e2a3d329a7542a13
Muhammad Tanveer <75263145+M-Tanveercs@users.noreply.github.com>
2023-10-20 13:03:41 +0500

    setup app.jsx file

--- Tech_Blog/src/App.jsx
+++ Tech_Blog/src/App.jsx
@@ -1,15 +1,46 @@
-
-
+import { useDispatch } from 'react-redux'
+import { useState,useEffect } from 'react'
+import authService from './appwrite/auth'
 import './App.css'
 
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components'
+import { Outlet } from 'react-router-dom'
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL );
+const [loading, setLoading] = useState(true)
+const dispatch=useDispatch()
 
-  return (
-    <><h1>Tech Blog Web site</h1>
-     
-    </>
-  )
+
+useEffect(() => {
+authService.getCurrentUser()
+.then((userData)=>{
+  if(userData){
+    dispatch(login(userData))
+  }
+dispatch(logout())
+
+})
+.finally(()=>setLoading(false))
+
+
+}, [])
+return (!loading? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+  <div className='w-full block'>
+<Header/>
+<main>
+ <Outlet/>
+</main>
+
+<Footer/>
+    
+  </div>
+  
+  
+  </div>
+):null)
+
+  
 }
 
 export default App
